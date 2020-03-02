f5fe5dd39bd1da968a4013831a14bed2a7aea22c
WUrunxiang <779720823@qq.com>
2020-03-02 14:55:31 +0800

    瞎导v2.0 完成基本要求 准备加入bugly

---
+++ app/src/main/java/com/example/mapaps/MyApplication.java
@@ -0,0 +1,47 @@
+package com.example.mapaps;
+
+import android.app.Application;
+
+import com.example.mapaps.activity.MainActivity;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+
+public class MyApplication extends Application {
+    public static final String APP_ID = "3bfc25f272";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /***** Beta高级设置 *****/
+        /**
+         * true表示app启动自动初始化升级模块; false不会自动初始化;
+         * 开发者如果担心sdk初始化影响app启动速度，可以设置为false，
+         * 在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
+         */
+        Beta.autoInit = true;
+        /**
+         * true表示初始化时自动检查升级; false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
+         */
+        Beta.autoCheckUpgrade = true;
+        /**
+         * 设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
+         */
+        Beta.upgradeCheckPeriod = 60 * 1000;
+        /**
+         * 设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
+         */
+        Beta.initDelay = 1 * 1000;
+        /**
+         * 已经确认过的弹窗在APP下次启动自动检查更新时会再次显示;
+         */
+        Beta.showInterruptedStrategy = true;
+        /**
+         * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗; 不设置会默认所有activity都可以显示弹窗;
+         */
+        Beta.canShowUpgradeActs.add(MainActivity.class);
+
+        //buggly初始化
+        Bugly.init(getApplicationContext(), APP_ID, true);
+    }
+}
---
+++ app/src/main/java/com/example/mapaps/adapter/myFileProvider.java
@@ -0,0 +1,6 @@
+package com.example.mapaps.adapter;
+
+import androidx.core.content.FileProvider;
+
+public class myFileProvider extends FileProvider {
+}
